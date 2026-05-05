34fdd446d3cd16c0a707aa1a251f720a7c1549f2
Yunus Bal <yunusbal@hotmail.com>
2026-05-05 16:23:21 +0100

    v1.0.6 binary distribution
    Adds built-in room info card with Navigate button (tap a room → card
    slides up with name + 'Yol tarifi al').

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobikiSDK",
-            url: "https://github.com/Mobiki/mobiki-map-sdk/releases/download/v1.0.5/MobikiSDK.xcframework.zip",
-            checksum: "40c6bedad2162a041bcf0fa679cbeb097d8f5120d50b691f5e56ad98336910c3"
+            url: "https://github.com/Mobiki/mobiki-map-sdk/releases/download/v1.0.6/MobikiSDK.xcframework.zip",
+            checksum: "f5dd9e9af3f49a1fa818d495daa8025d58071eb1b1f29ba9ea1292a5a6290548"
         )
     ]
 )
