6f84cd7430b26fbe31309f0366dda692dd0db534
Yunus Bal <yunusbal@hotmail.com>
2026-05-05 16:33:59 +0100

    v1.0.7 binary distribution
    Share button moved into the room info card; route-type picker and
    floating share dropped from chrome.

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobikiSDK",
-            url: "https://github.com/Mobiki/mobiki-map-sdk/releases/download/v1.0.6/MobikiSDK.xcframework.zip",
-            checksum: "f5dd9e9af3f49a1fa818d495daa8025d58071eb1b1f29ba9ea1292a5a6290548"
+            url: "https://github.com/Mobiki/mobiki-map-sdk/releases/download/v1.0.7/MobikiSDK.xcframework.zip",
+            checksum: "25f7eaea07cede9058da7a54e1630af557ab89dd5a1df21105071a602824085f"
         )
     ]
 )
